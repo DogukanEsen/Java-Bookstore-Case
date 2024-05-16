d1f9d721f41817c61d899bb933fd1076db4072c7
Satı Doğukan ESEN <esendogukan@hotmail.com>
2024-05-16 22:35:15 +0300

    Dependencies added

---
+++ BookStore/src/test/java/com/IWalletJavaCase/BookStore/BookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IWalletJavaCase.BookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
