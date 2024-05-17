392e38b6cf2f8b8dd209bec8827c808e1881ff04
Satı Doğukan ESEN <esendogukan@hotmail.com>
2024-05-18 02:19:07 +0300

    CartItem and Cart repository added

---
+++ BookStore/src/main/java/com/IWalletJavaCase/BookStore/repository/CartRepository.java
@@ -0,0 +1,10 @@
+package com.IWalletJavaCase.BookStore.repository;
+
+import com.IWalletJavaCase.BookStore.model.Cart;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CartRepository extends JpaRepository<Cart,Long> {
+    Optional<Cart> findByUserId(Long userId);
+}
